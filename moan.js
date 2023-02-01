5642604830e2845c596a7e442e1b7d3e4b6e2dd0
Mikail Thoriq <mikail.thoriq@gmail.com>
2023-02-01 12:32:51 +0700

    add button

--- moan.js
+++ moan.js
@@ -2,20 +2,25 @@ window.addEventListener('keydown', function(e) {
   console.log(e);
 })
 
+function startbutton(id) {
+  var audio = document.getElementById(id);
+  audio.currentTime = 0;
+  audio.play();
+}
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
   }
 
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
 
   const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
